72d5d5875fd119d7b5353be06dd43c017bb3b5ff
AesirOrb <144424360+AesirOrb@users.noreply.github.com>
2025-11-22 07:24:49 +0900

    Add files via upload

---
+++ Dalimernet Tweak.js
@@ -0,0 +1,72 @@
+/*
+==UserScript==
+@name         Dalimernet Tweak
+@version      1.2.1
+@description  Dalimernet Tweak
+@match        *://dlm16.net/*
+@run-at       document-start
+==/UserScript==
+*/
+
+(() => {
+	'use strict';
+
+	window.addEventListener('load', function () {
+		const pointhistory = document.querySelector('.pointhistory');
+		if (pointhistory) {
+			const links = pointhistory.getElementsByTagName('a');
+			for (const link of links) {
+				link.style.color = 'hsl(227, 18%, 25%)';
+				link.style.textDecoration = 'underline';
+			}
+		}
+
+		document.addEventListener('keydown', function (e) {
+			if (e.target instanceof HTMLInputElement || e.target instanceof HTMLSelectElement) return;
+
+			const activeDialog = document.querySelector('.app-dialog')?.classList.contains('active');
+
+			if (e.key.toLowerCase() === 'q' && !activeDialog) {
+				const btnShowMoreReview = document.querySelector('#fo_search > a:nth-child(7)');
+				if (btnShowMoreReview) return btnShowMoreReview.click();
+
+				const btnShowMore = document.querySelector('#app-board-search > div.app-dialog-container > div > form > div.tw-flex.tw-justify-end > a');
+				if (btnShowMore) return btnShowMore.click();
+
+				const btnSearch = document.querySelector('#board-list > div:nth-child(2) > div > a');
+				if (btnSearch) {
+					e.preventDefault();
+					btnSearch.click();
+					document.querySelector('.app-input-expand').focus();
+				}
+			}
+		});
+	});
+
+	document.addEventListener('click', function (e) {
+		const a = e.target.closest('a');
+		if (!a) return;
+
+		const url = new URL(a.href, this.location.origin);
+		if (url.searchParams.size) return;
+
+		switch (url.pathname) {
+			case '/board_SjQX31':
+				var href = '/category/458';
+				break;
+
+			case '/board_coJF70':
+				var href = '/category/493';
+				break;
+
+			case '/board_bJKb47':
+				var href = '/category/510';
+				break;
+		}
+
+		if (href) {
+			e.preventDefault();
+			this.location.href = url.pathname + href;
+		}
+	});
+})();
