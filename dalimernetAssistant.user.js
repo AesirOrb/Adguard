6937c417351439c6395f2275b2a8160f20935acb
AesirOrb <144424360+AesirOrb@users.noreply.github.com>
2025-11-25 14:42:28 +0900

    Bump version to 3.1.5 and minor adjustments

--- dalimernetAssistant.user.js
+++ dalimernetAssistant.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Dalimernet Assistant
-// @version      3.1.4
+// @version      3.1.5
 // @description  달리머넷에서 달리머 후기 카테고리 리다이렉트 및 정렬 기능, 'Q' 단축키로 검색/계속검색 클릭 기능이 추가되고 다크모드에서의 포인트 내역 스타일을 개선합니다.
 // @updateURL    https://raw.githubusercontent.com/AesirOrb/Adguard/refs/heads/main/dalimernetAssistant.user.js
 // @downloadURL  https://raw.githubusercontent.com/AesirOrb/Adguard/refs/heads/main/dalimernetAssistant.user.js
@@ -11,14 +11,14 @@
 (() => {
 	'use strict';
 
-	document.addEventListener('DOMContentLoaded', () => {
-		alert('loaded');
-		
-		addRedirectCategory();
-		addSortFunction();
-		addKeydownEvent();
-		fixPointHistory();
-	});
+	alert('loaded');
+
+	addRedirectCategory();
+	addSortFunction();
+	addKeydownEvent();
+	fixPointHistory();
+
+	document.addEventListener('DOMContentLoaded', () => {});
 })();
 
 function addRedirectCategory() {
@@ -177,4 +177,3 @@ function fixPointHistory() {
 		link.style.textDecoration = 'underline';
 	}
 }
-
